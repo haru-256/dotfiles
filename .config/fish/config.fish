4c83262a4a765a8502eec240d44df9225bcd8ef6
Yohei Okabayashi <yohei.kuro48@gmail.com>
2026-05-01 12:40:56 +0900

    Apply suggestion from @haru-256

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -107,7 +107,7 @@ set -gx CPPFLAGS -I/opt/homebrew/opt/openjdk/include
 # fzf
 # dracura
 # set -x FZF_DEFAULT_OPTS "--layout=reverse --border=rounded --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
-# catppuccin
+# catppuccin latte
 set -x FZF_DEFAULT_OPTS "\
 --layout=reverse \
 --border=rounded \
